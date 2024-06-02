fa377633969bc4752834cce9380e48315e763d81
jaggy-1010 <jaggy1010@protonmail.com>
2024-06-02 11:17:21 +0200

    Adding new test to EmployeeTests.cs

--- ChallengeApp.Tests/EmployeeTests.cs
+++ ChallengeApp.Tests/EmployeeTests.cs
@@ -29,4 +29,42 @@ public class EmployeeTests
         Assert.AreEqual("Chrobry",result);
     }
 
+    [Test]
+    public void CheckScoresStatisticsWhereEmployeeScored()
+    {
+        // arrange
+        Employee employee = new Employee("Stefan", " Batory");
+        employee.AddScore("1");
+        employee.AddScore("2");
+        employee.AddScore("4");
+        
+        // act
+        var statistics = employee.GetStatistics();
+        var resultAvg = Math.Round(statistics.Average, 2);
+        var resultMin = statistics.Min;
+        var resultMax = statistics.Max;
+        
+        // assert
+        Assert.AreEqual(Math.Round(2.33,2), resultAvg);
+        Assert.AreEqual(1.0, resultMin);
+        Assert.AreEqual(4.0, resultMax);
+    }
+
+    [Test]
+    public void CheckScoresStatistingsWhereEmployeeIsNotScored()
+    {
+        // arrange
+        Employee employee = new Employee("Kazimierz", "Jagiellończyk");
+        
+        // act
+        var statistics = employee.GetStatistics();
+        var resultAvg = Math.Round(statistics.Average, 2);
+        var resultMin = statistics.Min;
+        var resultMax = statistics.Max;
+        
+        // assert
+        Assert.AreEqual(Math.Round(0.0, 2), resultAvg);
+        Assert.AreEqual(0.0, resultMin);
+        Assert.AreEqual(0.0, resultMax);
+    }
 }
